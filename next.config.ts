327ab6c9b72b4d0a25ee475fff7002ad6b663f14
d.ermakov <d.ermakov@univertv.ru>
2026-06-08 14:07:10 +0300

    hw-2: fix

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    images: {
-        dangerouslyAllowLocalIP: process.env.NODE_ENV === 'development',
-        remotePatterns: [{
-            hostname: 'localhost',
-            port: '4000',
-            protocol: 'http',
-            pathname: '/**',
-        }],
-    },
     compiler: {
         styledComponents: true,
     },
