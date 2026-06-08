60b4b62bdc471b80ddb385541f9d4618c1a1edcc
d.ermakov <d.ermakov@univertv.ru>
2026-06-08 14:06:39 +0300

    hw-2: fix

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    images: {
+        dangerouslyAllowLocalIP: process.env.NODE_ENV === 'development',
+        remotePatterns: [{
+            hostname: 'localhost',
+            port: '4000',
+            protocol: 'http',
+            pathname: '/**',
+        }],
+    },
     compiler: {
         styledComponents: true,
     },
     typescript: {
-        ignoreBuildErrors: true,  // ← временно, пока Turbopack не пофиксят
+        ignoreBuildErrors: true,
     },
 };
 
